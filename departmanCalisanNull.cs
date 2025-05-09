93f5274a55476f03dbf437c4364f035321768776
Berre <90646787+Berresu@users.noreply.github.com>
2025-05-09 10:29:31 +0300

    Create departmanCalisanNull.cs

---
+++ departmanCalisanNull.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.Program;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public class Departman
+        {
+            public string Ad {  get; set; }
+            public List<Calisan> Calisanlar { get; set; } = new List<Calisan>();
+
+            public Departman(string ad)
+            {
+                Ad = ad;
+            }
+
+            public void CalisanEkle(Calisan calisan)
+            {
+                Calisanlar.Add(calisan);
+            }
+
+            public void DepartmandakiCalisanlariGoster()
+            {
+                Console.WriteLine($"{Ad} Departmanındaki Çalışanlar:");
+                foreach (var calisan in Calisanlar)
+                {
+                    Console.WriteLine($"- {calisan.Ad}");
+                }
+            }
+        }
+
+        public class Calisan
+        {
+            public string Ad { get; set; }
+            public Departman Departman { get; set; }
+
+            public Calisan(string ad)
+            {
+                Ad = ad;
+            }
+
+            public void DepartmanAta(Departman departman)
+            {
+                Departman = departman;
+            }
+
+            public void CalisanBilgileriniGoster()
+            {
+                Console.WriteLine($"Çalışan Adı: {Ad}");
+                if (Departman.Ad != null)
+                {
+                    Console.WriteLine($"Departmanı: {Departman.Ad}");
+                }
+                else
+                {
+                    Console.WriteLine("Çalışan Herhangi Bir Departmana Bağlı Değil.");
+                }
+            }
+        }
+
+        public class AggergationOrnek
+        {
+            static void Main(string[] args)
+            {
+                Departman yazilimDepartmani = new Departman("Yazılım Departmanı");
+                Calisan berre = new Calisan("Berre");
+                Calisan alper = new Calisan("Alper");
+
+                yazilimDepartmani.CalisanEkle(berre);
+                yazilimDepartmani.CalisanEkle(alper);
+                berre.DepartmanAta(yazilimDepartmani);
+                alper.DepartmanAta(yazilimDepartmani);
+
+                yazilimDepartmani = null;
+                berre.CalisanBilgileriniGoster();
+                alper.CalisanBilgileriniGoster();
+            }
+        }
+    }
+}
